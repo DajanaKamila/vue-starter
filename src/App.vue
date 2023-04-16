aef8a6fc7daf33846d4e3a9ec1d50ca14475892d
dominiak <dominiak@d17-430-pc-17.ipa.ii.agh.edu.pl>
2023-04-16 11:05:19 +0200

    First commit according to the instruction

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,57 @@
 <template>
   <div class="my-app">
-    <h1>Hello MWO!</h1>
-  </div>
+<!--    <h1>Twój email to {{email}}</h1>-->
+    <h1>Witaj w systemie do zapisów na zajęcia</h1>
+    <div v-if="loggedIn == false">
+        <p>Zaloguj się e-mailem
+        <input type="email" v-model="email">
+        <button @click="login()">Wchodzę</button></p></div>
+    <div v-if="loggedIn == true">
+        <p>Witaj {{email}}</p>
+        <p id="p2" @click="logout()">Wyloguj</p></div>
+    </div>
+
+
+<!--    <div v-if="email.length < 10">Ale masz krótki adres!</div>-->
+<!--    <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>-->
+<!--    <div v-else>Twój adres e-mail jest stanowczo za długi.</div>-->
+<!--    <div v-if="!email.includes('@')" > To nie jest adres email! </div>-->
+<!--    {{email.length}}-->
+
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      email: '',
+      password: '',
+      loggedIn: false,
+    };
+  },
+  methods: {
+    alertMyEmail() {
+      alert(this.email);
+    },
+    login(){
+      this.loggedIn = true;
+    },
+    logout(){
+      this.loggedIn = false
+    }
+  }
 }
 </script>
 
 <style>
 .my-app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
 }
+
+#p2 {
+  color : dodgerblue;
+}
+
 </style>
